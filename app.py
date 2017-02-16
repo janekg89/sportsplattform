b47a727b58bd97c9137c167f3602145be6335348
janekg89 <janekg89@hotmail.de>
2017-02-16 22:18:47 +0100

    updated config settings

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ app = Flask(__name__)
 # config
 import os
 app.config.from_object(os.environ['APP_SETTINGS'])
-print os.environ['APP_SETTINGS']
 # create the sqlalchemy object
 db = SQLAlchemy(app)
 
